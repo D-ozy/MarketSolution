261a0dd8b964b2db84ea0665ae51e799acfc67e9
Ddude <ratatouille395@gmail.com>
2025-06-18 22:21:21 +0300

    коммит перед работой

--- market/Middlewares/MainMiddleware.cs
+++ market/Middlewares/MainMiddleware.cs
@@ -1,5 +1,6 @@
 ﻿using market.DbContextFolder;
 using MyMarketLibrary.Models;
+using System.Text.RegularExpressions;
 
 namespace market.Middlewares
 {
@@ -18,13 +19,20 @@ namespace market.Middlewares
             HttpRequest request = context.Request;
             PathString path = request.Path;
 
-            if(path == "/main/user/get" && request.Method == "GET")
+            string expressionForGuid = @"^/main/item/add/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}$";
+
+            if (path == "/main/user/get" && request.Method == "GET")
             {
                 await GetUser(response, request);
             }
-            else if(path == "/main/item/add" && request.Method == "POST")
+            else if(path == "/main/item/get" && request.Method == "GET")
             {
-                await AddItem(response, request);
+                await GetAllItems(response);
+            }
+            else if(Regex.IsMatch(path, expressionForGuid) && request.Method == "POST")
+            {
+                string? id = path.Value?.Split("/")[3];
+                await AddItem(response, request, id);
             }
             else
             {
@@ -47,13 +55,55 @@ namespace market.Middlewares
             }
         }
 
-        private async Task AddItem(HttpResponse response, HttpRequest request)
+        private async Task GetAllItems(HttpResponse response)
+        {
+            using(MarketDbContext db = new MarketDbContext())
+            {
+                await response.WriteAsJsonAsync(db.items);
+            }
+        }
+
+        private async Task AddItem(HttpResponse response, HttpRequest request, string? itemId)
         {
             string userIdStr = request.Headers["X-User-Id"];
 
-            using(MarketDbContext db = new MarketDbContext())
+            using (MarketDbContext db = new MarketDbContext())
             {
                 Cart cart = db.carts.FirstOrDefault(c => c.user_id == userIdStr);
+
+                if(cart == null)
+                {
+                    response.StatusCode = 404;
+                    await response.WriteAsJsonAsync(new { message = "The cart does not exist" });
+                    return;
+                }
+
+                Item item = db.items.FirstOrDefault(i => i.id == itemId);
+
+                if (item == null)
+                {
+                    response.StatusCode = 404;
+                    await response.WriteAsJsonAsync(new { message = "The item does not exist" });
+                    return;
+                }
+
+                var existingItem = db.carts_item.FirstOrDefault(ci => ci.cart_id == cart.id && ci.item_id == itemId);
+
+                if (existingItem != null)
+                {
+                    existingItem.quantity += 1;
+                }
+                else
+                {
+                    CartsItem cartsItem = new CartsItem() { id = Guid.NewGuid().ToString(), cart_id = cart.id, quantity = 1, item_id = itemId };
+                    db.carts_item.Add(cartsItem);
+                }
+
+                db.SaveChanges();
+
+                response.StatusCode = 200;
+
+                Console.WriteLine("Item added to cart");
             }
         }
     }
